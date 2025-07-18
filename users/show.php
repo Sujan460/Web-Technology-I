1093ddd3adf0a22cd5dc54a51a8d06480b2db241
Sujan460 <khanalsujan460@gmail.com>
2025-07-18 13:47:35 +0545

    error fixed

--- users/show.php
+++ users/show.php
@@ -8,7 +8,7 @@ require('../config/db.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Document</title>
+    <title>CRUD</title>
 </head>
 <body>
 <main>
